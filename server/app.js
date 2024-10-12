e1dec799b002f239292b4e2393de22e2f67c762b
harshmax <117888618+Harsh-cyber005@users.noreply.github.com>
2024-10-13 00:21:29 +0530

    cors

--- server/app.js
+++ server/app.js
@@ -14,7 +14,10 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: 'GET, POST, PUT, DELETE',
+}));
 
 app.get('/', (req, res) => {
     return res.send('Hello World!');
